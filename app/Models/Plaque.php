28137cda7097cd1517fb59638240e130e7d20d32
Matt Inamdar <matt@ayup.agency>
2019-06-18 20:23:19 +0100

    Added casts for models

--- app/Models/Plaque.php
+++ app/Models/Plaque.php
@@ -5,6 +5,15 @@ namespace App\Models;
 class Plaque extends Model
 {
     /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'date_unveiled' => 'date',
+    ];
+
+    /**
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
     public function tickets()
--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -5,6 +5,15 @@ namespace App\Models;
 class Ticket extends Model
 {
     /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'closed_at' => 'datetime',
+    ];
+
+    /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
     public function plaque()
