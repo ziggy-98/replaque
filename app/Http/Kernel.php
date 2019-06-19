c2ffee88809147b5855c2aa6917c55c8bb9bda24
Matt Inamdar <matt@ayup.agency>
2019-06-19 11:49:58 +0100

    Added CORS support

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,8 +37,8 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
             'bindings',
+            \Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
