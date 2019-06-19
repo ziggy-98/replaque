760249c2ed7cebab5d4825723538e2079f4596a3
Luke Zigler <luke@reallyquite.com>
2019-06-19 15:43:27 +0100

    added the structure of the plaques and tickets

--- app/Http/Resources/PlaqueResource.php
+++ app/Http/Resources/PlaqueResource.php
@@ -16,6 +16,20 @@ class PlaqueResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'name' => $this->name,
+            'address_line_1' => $this->address_line_1,
+            'address_line_2' => $this->address_line_2,
+            'address_line_3' => $this->address_line_3,
+            'city' => $this->city,
+            'postcode' => $this->postcode,
+            'lat' => $this->lat,
+            'lng' => $this->lng,
+            'unveiler' => $this->unveiler,
+            'date_unveiled' => $this->date_unveiled->toIso8601String(),
+            'sponsor' => $this->sponsor,
+            'comments' => $this->comments,
+            'created_at' => $this->created_at->toIso8601String(),
+            'updated_at' => $this->updated_at->toIso8601String(),
             // TODO: Complete rest of fields.
             // https://laravel.com/docs/5.8/eloquent-resources#writing-resources
             // TODO: Return dates/datetimes as ISO8601.
--- app/Http/Resources/TicketResource.php
+++ app/Http/Resources/TicketResource.php
@@ -16,6 +16,10 @@ class TicketResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'plaque_id' => $this->plaque_id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'closed_at' => $this->closed_at->toIso8601String(),
             // TODO: Complete rest of fields.
             // https://laravel.com/docs/5.8/eloquent-resources#writing-resources
             // TODO: Return dates/datetimes as ISO8601.
